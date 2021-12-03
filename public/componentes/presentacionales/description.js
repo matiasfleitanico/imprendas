76805923faaae3516537336e8fe2e345a9b81caa
Mati Fley <mattfleita@gmail.com>
2021-12-03 16:38:28 -0300

    changes

---
+++ public/componentes/presentacionales/description.js
@@ -0,0 +1,16 @@
+import styles from '../../../styles/Home.module.scss'
+import Image1 from '../parts/image1'
+
+export default function Description() {
+    return (
+    <section className={styles.description}>
+        <div> 
+            <h2>asdasdasd</h2>
+        </div>
+        <div className="container-description"> 
+            <h1>fsdfsd</h1>
+        </div>
+    </section>
+    )
+  }
+  
