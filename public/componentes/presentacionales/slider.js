e5b1f4909f4a4bc321a23276c407181a3be84d68
Matias Fleita <matiasfleitanico@gmail.com>
2022-01-06 19:23:58 -0300

    ultimos cambios

--- public/componentes/presentacionales/slider.js
+++ public/componentes/presentacionales/slider.js
@@ -1,53 +1,53 @@
 import styles from '../../../styles/Home.module.scss'
 
-// export default function Slider() {
+export default function Slider() {
 
-//     return (
-//     <section id="slider">
-//         <div className={styles.slider}>        
-        <img src="../../img/logo.png" className={styles.imgSlider}/>
-//         </div>
-
-//     </section>
-//     )
-//   }
-import React, { Component } from "react";
-import Slider from "react-slick";
-
-export default class FocusOnSelect extends Component {
-  render() {
-    const settings = {
-      focusOnSelect: true,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      speed: 500
-    };
     return (
-      <div>
-        <h2>FocusOnSelect</h2>
-        <div>Click on any slide to select and make it current slide</div>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
-    );
+    <section id="slider">
+        <div className={styles.slider}>        
+        <img src="../../img/logo.png" className={styles.imgSlider}/>
+        </div>
+
+    </section>
+    )
   }
-}
+// import React, { Component } from "react";
+// import Slider from "react-slick";
+
+// export default class FocusOnSelect extends Component {
+//   render() {
+//     const settings = {
+//       focusOnSelect: true,
+//       infinite: true,
+//       slidesToShow: 3,
+//       slidesToScroll: 1,
+//       speed: 500
+//     };
+//     return (
+//       <div>
+//         <h2>FocusOnSelect</h2>
+//         <div>Click on any slide to select and make it current slide</div>
+//         <Slider {...settings}>
+//           <div>
+//             <h3>1</h3>
+//           </div>
+//           <div>
+//             <h3>2</h3>
+//           </div>
+//           <div>
+//             <h3>3</h3>
+//           </div>
+//           <div>
+//             <h3>4</h3>
+//           </div>
+//           <div>
+//             <h3>5</h3>
+//           </div>
+//           <div>
+//             <h3>6</h3>
+//           </div>
+//         </Slider>
+//       </div>
+//     );
+//   }
+// }
